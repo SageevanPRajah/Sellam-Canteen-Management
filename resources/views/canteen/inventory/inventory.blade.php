21a28bdbc34271ca358ea538ed7b0bcbd8ef0eb6
IT22094704 <it22094704@my.sliit.lk>
2025-04-07 22:04:19 +0530

    feat: Update inventory management functionality with action handling and balance calculation

--- resources/views/canteen/inventory/inventory.blade.php
+++ resources/views/canteen/inventory/inventory.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <h1>Inventory for Show: {{ $show->movie_name }} on {{ $show->date }} at {{ $show->time }}</h1>
+    <h1>Inventory for Show on {{ $show->date }} at {{ $show->time }}</h1>
     <form action="{{ route('canteen.inventory.update', $show->id) }}" method="POST">
         @csrf
         <table>
@@ -15,30 +15,32 @@
             </thead>
             <tbody>
                 @foreach($products as $product)
-                @php
-                    $inventory = $inventories->get($product->id);
-                    $initial = $inventory ? $inventory->initial_stock : $product->stock_count;
-                    $refill  = $inventory ? $inventory->refill_stock : 0;
-                    $final   = $inventory ? $inventory->final_stock : $product->stock_count;
-                    $sold    = ($initial + $refill) - $final;
-                @endphp
-                <tr>
-                    <td>{{ $product->name }}</td>
-                    <td>{{ $product->stock_count }}</td>
-                    <td>
-                        <input type="number" name="inventories[{{ $product->id }}][initial_stock]" value="{{ $initial }}" required>
-                    </td>
-                    <td>
-                        <input type="number" name="inventories[{{ $product->id }}][refill_stock]" value="{{ $refill }}" required>
-                    </td>
-                    <td>
-                        <input type="number" name="inventories[{{ $product->id }}][final_stock]" value="{{ $final }}" required>
-                    </td>
-                    <td>{{ $sold }}</td>
-                </tr>
+                    @php
+                        $inventory = $inventories->get($product->id);
+                        $initial = $inventory ? $inventory->initial_stock : $product->stock_count;
+                        $refill  = $inventory ? $inventory->refill_stock : 0;
+                        $final   = $inventory ? $inventory->final_stock : $product->stock_count;
+                        $sold    = ($initial + $refill) - $final;
+                    @endphp
+                    <tr>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->stock_count }}</td>
+                        <td>
+                            <input type="number" name="inventories[{{ $product->id }}][initial_stock]" value="{{ $initial }}" required>
+                        </td>
+                        <td>
+                            <input type="number" name="inventories[{{ $product->id }}][refill_stock]" value="{{ $refill }}" required>
+                        </td>
+                        <td>
+                            <input type="number" name="inventories[{{ $product->id }}][final_stock]" value="{{ $final }}" required>
+                        </td>
+                        <td>{{ $sold }}</td>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
-        <button type="submit">Update Inventory</button>
+        <!-- Two buttons: one for updating stock and one for updating inventory (and recording income) -->
+        <button type="submit" name="action" value="update_stock">Update Stock</button>
+        <button type="submit" name="action" value="update_inventory">Update Inventory</button>
     </form>
 </x-app-layout>
--- resources/views/canteen/inventory/select_show.blade.php
+++ resources/views/canteen/inventory/select_show.blade.php
@@ -1,11 +1,17 @@
 <x-app-layout>
     <h1>Select Show for Inventory Management</h1>
+    <form action="{{ route('canteen.inventory.selectShow') }}" method="GET">
+        <label for="date">Select Date:</label>
+        <input type="date" name="date" id="date">
+        <button type="submit">Filter Shows</button>
+    </form>
+
     <ul>
         @foreach($shows as $show)
-        <li>
-            {{ $show->date }} - {{ $show->time }} - {{ $show->movie_name }}
-            <a href="{{ route('canteen.inventory.show', $show->id) }}">Manage Inventory</a>
-        </li>
+            <li>
+                {{ $show->date }} - {{ $show->time }}
+                <a href="{{ route('canteen.inventory.show', $show->id) }}">Manage Inventory</a>
+            </li>
         @endforeach
     </ul>
 </x-app-layout>
